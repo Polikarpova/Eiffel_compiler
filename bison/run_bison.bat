e02e3cc75774c36e51e6bc833e852aa7d7f44056
den1s0v <hello-lumen@ya.ru>
2018-10-11 00:09:11 +0400

    win_flex_bison/ folder added & BAT for running win_bison.exe from bison/

---
+++ bison/run_bison.bat
@@ -0,0 +1,2 @@
+..\win_flex_bison\win_bison ..\flex\flex\eiffel_bison.y -v 
+REM flags: 		-g : generate dot graph
---
+++ bison/run_dot.bat
@@ -0,0 +1,2 @@
+"c:\Program Files\Graphviz2.38\bin\dot.exe"  -Tpng "eiffel_bison.dot" -o "graph_out.png"
+pause
