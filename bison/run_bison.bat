42150af425923026cbbece92aaad3c3677fe9d7e
den1s0v <hello-lumen@ya.ru>
2018-10-12 02:12:53 +0400

    Removed .bat files from tracking

--- bison/run_bison.bat
+++
@@ -1,2 +0,0 @@
-..\win_flex_bison\win_bison ..\flex\flex\eiffel_bison.y -v -g
-REM flags: 		-g : generate dot graph
--- bison/run_dot.bat
+++
@@ -1,2 +0,0 @@
-"c:\Program Files (x86)\Graphviz2.38\bin\dot.exe"  -Tsvg "eiffel_bison.dot" -o "graph_out.png"
-pause
