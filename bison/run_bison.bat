c3f26f4c363314877ad73d609ac1d53fa4aee1c2
Polikarpova <policarpova97@mail.ru>
2018-10-11 20:21:26 +0300

    Add *.bat into .gitingnore

--- bison/run_bison.bat
+++ bison/run_bison.bat
@@ -1,2 +1,2 @@
-..\win_flex_bison\win_bison ..\flex\flex\eiffel_bison.y -v 
+..\win_flex_bison\win_bison ..\flex\flex\eiffel_bison.y -v -g
 REM flags: 		-g : generate dot graph
--- bison/run_dot.bat
+++ bison/run_dot.bat
@@ -1,2 +1,2 @@
-"c:\Program Files\Graphviz2.38\bin\dot.exe"  -Tpng "eiffel_bison.dot" -o "graph_out.png"
+"c:\Program Files (x86)\Graphviz2.38\bin\dot.exe"  -Tsvg "eiffel_bison.dot" -o "graph_out.png"
 pause
