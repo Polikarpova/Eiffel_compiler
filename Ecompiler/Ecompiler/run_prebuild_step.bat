d834504ec96221272b164aeb15b1937ae98aded1
den1s0v <hello-lumen@ya.ru>
2018-12-10 00:20:42 +0400

    added .bat file that compiles java sources, & added as prebuild custom step

---
+++ Ecompiler/Ecompiler/run_prebuild_step.bat
@@ -0,0 +1,18 @@
+echo off
+echo  Compliling /java/*/* sources ...
+REM add all our paths to /bin
+set PATH=c:\Program Files\Java\jdk1.8.0_171\bin\;c:\Program Files\Java\jdk1.8.0_121\bin\;%PATH%
+
+REM javac -sourcepath <path>         Specify where to find input source files
+
+javac java/eiffel/*.java   java/rtl/*.java 
+REM javac -sourcepath java/eiffel/*.java
+REM javac  java/*.java
+ REM -verbose > java/jc_output.txt
+REM javac -help
+REM javac -version
+REM javac -sourcepath java/
+
+rem pause
+
+REM exit
