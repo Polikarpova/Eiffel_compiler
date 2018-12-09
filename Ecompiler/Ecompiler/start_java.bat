fa4570450b7b6402b30444170b8086e799d4179b
den1s0v <hello-lumen@ya.ru>
2018-12-10 00:24:00 +0400

    added .bat that runs `HELLO_WORLD.class` and prints in console!

---
+++ Ecompiler/Ecompiler/start_java.bat
@@ -0,0 +1,16 @@
+echo off
+echo ======= Starting a java class file =======
+REM add all our paths to /bin
+set PATH=c:\Program Files\Java\jdk1.8.0_171\bin\;c:\Program Files\Java\jdk1.8.0_121\bin\;%PATH%
+
+
+
+REM USAGE: java -classpath <c:\path/to/search;search/class/files>  <class name to start: package1.package2.class>
+
+java -classpath java  eiffel.HELLO_WORLD
+REM так тоже работает:
+REM java -classpath c:\Dev\Eiffel\repo\Eiffel_compiler\Ecompiler\Ecompiler\java\;java  eiffel.HELLO_WORLD
+
+rem pause
+
+REM exit
