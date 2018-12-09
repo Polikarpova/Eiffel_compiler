0afd0d426e8931c61f3183d6bfa6eb18d1fc4c3b
den1s0v <hello-lumen@ya.ru>
2018-12-10 00:22:30 +0400

    impoved class viewer .bat: use both paths to java-bin folder on mine & Ann` machines

--- jvm/view_class.bat
+++ jvm/view_class.bat
@@ -3,7 +3,8 @@ echo  =
 echo ======= View %~nx1% =======
 echo  =  
 
-set PATH=c:\Program Files\Java\jdk1.8.0_171\bin\;%PATH%
+REM add all our paths to /bin
+set PATH=c:\Program Files\Java\jdk1.8.0_171\bin\;c:\Program Files\Java\jdk1.8.0_121\bin\;%PATH%
 javap -c -s -verbose %1
 
 rem pause
