d6d01305479e5be57ee6cf0ae0270ac9c08b21bf
den1s0v <hello-lumen@ya.ru>
2018-12-10 00:19:10 +0400

    added USER (test) sources for classes: eiffel.HELLO_WORLD

---
+++ Ecompiler/Ecompiler/java/eiffel/HELLO_WORLD.java
@@ -0,0 +1,23 @@
+package eiffel;
+
+/**
+ *
+ * @author Пользователь
+ */
+public class HELLO_WORLD {
+    public HELLO_WORLD()
+	{
+		
+	}
+
+    public void make()
+	{
+		rtl.Io.put_string("Hello world!");
+		// System.out.print("Hello world!");
+	}
+	
+	public static void main(java.lang.String args[])
+	{
+		(new HELLO_WORLD()).make();
+	}
+}
