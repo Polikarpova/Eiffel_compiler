6b2d2883450efa1b8e459dc16880f6b37d87b387
den1s0v <hello-lumen@ya.ru>
2018-12-10 00:23:05 +0400

    custom .bat that generates view for `HELLO_WORLD.class`

---
+++ Ecompiler/Ecompiler/java/eiffel/view_HELLO_WORLD.bat
@@ -0,0 +1,5 @@
+rem dir:  c:\Dev\Eiffel\repo\Eiffel_compiler\Ecompiler\Ecompiler\java\eiffel\
+
+cd ../..
+
+..\..\jvm\view_class.bat     java\eiffel\HELLO_WORLD.class   >   java\eiffel\HELLO_WORLD.class.javap
