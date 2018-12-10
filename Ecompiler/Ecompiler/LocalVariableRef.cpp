ba43effef053f5e05af0d1cb859453bf5e56e778
den1s0v <hello-lumen@ya.ru>
2018-12-10 22:48:55 +0400

    LocalVariableRef - выраженние-ссылка на переменную в методе

--- Ecompiler/Ecompiler/LocalVariableRef.cpp
+++ Ecompiler/Ecompiler/LocalVariableRef.cpp
@@ -1,11 +1,39 @@
 #include "LocalVariableRef.h"
+#include "LocalVariable.h"
 
 
 LocalVariableRef::LocalVariableRef(void)
+	: Expression()
 {
+	this->locVar = 0;
 }
 
 
 LocalVariableRef::~LocalVariableRef(void)
 {
 }
+
+/*static*/ LocalVariableRef* LocalVariableRef::create(Method* mtd, LocalVariable* locVar) {
+	LocalVariableRef* lvr = new LocalVariableRef();
+	lvr->currentMethod = mtd;
+	lvr->locVar = locVar;
+	lvr->type = locVar->type;
+	return lvr;
+}
+
+///*static*/ LocalVariableRef* LocalVariableRef::createRtlClassRef(Method* mtd, const QString& uncasedName)
+//{
+//	MetaClass* static_class = EiffelProgram::findClass( uncasedName.toUpper() );
+//
+//	if(static_class && static_class->isStatic)
+//	{
+//		LocalVariableRef* lvr = new LocalVariableRef();
+//		lvr->currentMethod = mtd;
+//		lvr->locVar = 0;
+//		// ! lvr->type = locVar->type;
+//		return lvr;
+//		
+//	}
+//
+//	return 0;
+//}
--- Ecompiler/Ecompiler/LocalVariableRef.h
+++ Ecompiler/Ecompiler/LocalVariableRef.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "Expression.h"
 
+class LocalVariable;
+
 class LocalVariableRef : public Expression
 {
 
@@ -9,5 +11,9 @@ public:
 	LocalVariableRef(void);
 	~LocalVariableRef(void);
 
+	LocalVariable* locVar;
+
+	static LocalVariableRef* create(Method* mtd, LocalVariable* locVar);
+	//static LocalVariableRef* createRtlClassRef(Method* mtd, const QString& uncasedName);
 
 };
