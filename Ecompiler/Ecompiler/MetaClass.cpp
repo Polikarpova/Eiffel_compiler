5d862a5068f5e7bfb6e9f4cd5b74808b01fb8e83
den1s0v <hello-lumen@ya.ru>
2018-12-15 21:54:56 +0400

    comments: parts of class bytecode

--- Ecompiler/Ecompiler/MetaClass.cpp
+++ Ecompiler/Ecompiler/MetaClass.cpp
@@ -359,7 +359,24 @@ bool MetaClass::generateCode(const QDir& code_dir)
 
 	ByteCode bc;
 
-	// ...
+ // ClassFile {
+ //	  u4 magic;
+ //   u2 minor_version;
+ //   u2 major_version;
+ //   u2 constant_pool_count;
+ //   cp_info constant_pool[constant_pool_count-1];
+ //   u2 access_flags;
+ //   u2 this_class;
+ //   u2 super_class;
+ //   u2 interfaces_count;
+ //   u2 interfaces[interfaces_count];
+ //   u2 fields_count;
+ //   field_info fields[fields_count];
+ //   u2 methods_count;
+ //   method_info methods[methods_count];
+ //   u2 attributes_count;
+ //   attribute_info attributes[attributes_count];
+ // }
 
 
 	// записываем байт-код в файл
