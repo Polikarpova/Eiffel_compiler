2131628ad5187e3656ba5d076981c8d8df58dfb9
den1s0v <hello-lumen@ya.ru>
2018-12-18 22:48:54 +0400

    implemented `FieldRef::toByteCode()` & bugs fixed

--- Ecompiler/Ecompiler/FieldRef.cpp
+++ Ecompiler/Ecompiler/FieldRef.cpp
@@ -1,6 +1,7 @@
 #include "FieldRef.h"
 
 #include "Field.h"
+#include "Method.h"
 
 FieldRef::FieldRef(void)
 	: Expression()
@@ -43,38 +44,38 @@ FieldRef::~FieldRef(void)
 	jc.type = UTF8_VALUE;
 	buffer = fr->field->metaClass->name();
 	jc.value.utf8 = & buffer;
-	short int class_utf8 = fr->field->metaClass->constantTable.put(jc);
+	short int class_utf8 = fr->currentMethod->metaClass->constantTable.put(jc);
 		
 	// Class Constant
 	jc.type = CLASS_N;
 	jc.value.class_const = class_utf8;
-	short int class_class = fr->field->metaClass->constantTable.put(jc);
+	short int class_class = fr->currentMethod->metaClass->constantTable.put(jc);
 
 	//-----------------Name&Type-----------------//
 	//имя поля
 	jc.type = UTF8_VALUE;
 	buffer = fr->field->javaName;
 	jc.value.utf8 = &buffer;
-	short int field_name_utf8 = fr->field->metaClass->constantTable.put(jc);
+	short int field_name_utf8 = fr->currentMethod->metaClass->constantTable.put(jc);
 	
 	//дескриптор
 	jc.type = UTF8_VALUE;
 	buffer = fr->field->getDescriptor();
 	jc.value.utf8 = &buffer;
-	short int field_descriptor_utf8 = fr->field->metaClass->constantTable.put(jc);
+	short int field_descriptor_utf8 = fr->currentMethod->metaClass->constantTable.put(jc);
 
 	//Name&Type Constant
 	jc.type = NAME_AND_TYPE;
 	jc.value.name_and_type[UTF8_NAME] = field_name_utf8;
 	jc.value.name_and_type[UTF8_DESCR] = field_descriptor_utf8;
-	short int field_name_and_type = fr->field->metaClass->constantTable.put(jc);
+	short int field_name_and_type = fr->currentMethod->metaClass->constantTable.put(jc);
 
 	//-----------------FieldRef-----------------//
 	//FieldRef Constant
 	jc.type = FIELD_REF;
 	jc.value.field_ref[CONST_CLASS] = class_class;
 	jc.value.field_ref[CONST_NAMEnTYPE] = field_name_and_type;
-	fr->fieldref_constN = fr->field->metaClass->constantTable.put(jc);
+	fr->fieldref_constN = fr->currentMethod->metaClass->constantTable.put(jc);
 
 	return fr;
 }
@@ -82,17 +83,23 @@ FieldRef::~FieldRef(void)
 
 ByteCode& FieldRef::toByteCode(ByteCode &bc)
 {
-	if(this->isLeftValue())
+	// load a reference to an object ...
+	// call qualification as this->left
+	if(this->left)
+		this->left->toByteCode(bc); // load qualification
+	else
+		bc.aload_0(); // load Current
+
+
+	if(this->isLeftValue()) // eiffel code:   left := right
 	{
-		//bc.invokespecial(methodref_constN,
-		//	this->calledMethod->exactNumberOfArgs(),
-		//	this->calledMethod->isVoid());
+		this->right->toByteCode(bc); // load right value to store in the field
+		bc.putfield(this->fieldref_constN);
 	}
 	else
 	{
-		//bc.invokevirtual(methodref_constN,
-		//	this->calledMethod->exactNumberOfArgs(),
-		//	this->calledMethod->isVoid());
+		bc.getfield(this->fieldref_constN);
 	}
+
 	return bc;
 }
