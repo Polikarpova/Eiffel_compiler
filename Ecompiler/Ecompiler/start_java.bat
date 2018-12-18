0b1b48b652d2f2df987011961e009247b49f1fcc
den1s0v <hello-lumen@ya.ru>
2018-12-19 01:49:05 +0400

    HELLO WORLD работает! Запускать `start_java.bat` в сеансе cmd

--- Ecompiler/Ecompiler/start_java.bat
+++ Ecompiler/Ecompiler/start_java.bat
@@ -7,7 +7,7 @@ set PATH=c:\Program Files\Java\jdk1.8.0_20\bin\;c:\Program Files\Java\jdk1.8.0_1
 
 REM USAGE: java -classpath <c:\path/to/search;search/class/files>  <class name to start: package1.package2.class>
 
-java -classpath java  eiffel.HELLO_WORLD
+java -classpath java;out  eiffel.HELLO_WORLD
 REM так тоже работает:
 REM java -classpath c:\Dev\Eiffel\repo\Eiffel_compiler\Ecompiler\Ecompiler\java\;java  eiffel.HELLO_WORLD
 
