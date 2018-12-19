239187cf96bbdbcfe48e6f3c227cdea69084bf8e
den1s0v <hello-lumen@ya.ru>
2018-12-20 01:50:33 +0400

    EiffelArray - тип "класса" ARRAY

--- Ecompiler/Ecompiler/EiffelArray.h
+++ Ecompiler/Ecompiler/EiffelArray.h
@@ -1,18 +1,38 @@
 #pragma once
-#include "EiffelType.h"
+#include "EiffelClass.h"
 
-class EiffelArray : public EiffelType
+
+/** Массив с произвольным типом элемента.
+	Доступные операции:
+	arr[i]
+	arr[i] := val
+	arr.
+*/
+class EiffelArray : public EiffelClass
 {
 
 public:
 	
-	EiffelArray(void) {};
-	EiffelArray(EiffelType * elementType) {
+	EiffelArray(void)
+		: EiffelClass(EiffelProgram::currentProgram->findClass("ARRAY"))
+	{
+		this->elementType = NULL;
+	};
+
+	EiffelArray(EiffelType * elementType)
+		: EiffelClass(EiffelProgram::currentProgram->findClass("ARRAY"))
+	{
 		this->elementType = elementType;
 	};
 	~EiffelArray(void) {};
 
 	EiffelType * elementType;
 
+	virtual bool isReference() const { return true; }
+	virtual bool isArray() const { return true; }
+
 	QString descriptor() { return QString("[%1").arg(elementType->descriptor()); }
+	QString toReadableString() const {return QString("ARRAY[%1]").arg(elementType->toReadableString());};
+
+
 };
