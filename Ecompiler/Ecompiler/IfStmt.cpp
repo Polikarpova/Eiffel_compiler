5a070cd6d23635eeb7cd7e8eb2ac6366e463705a
Polikarpova <policarpova97@mail.ru>
2018-12-20 12:33:19 +0300

    Подправили псевдокод ifStmt::toByteCode

--- Ecompiler/Ecompiler/IfStmt.cpp
+++ Ecompiler/Ecompiler/IfStmt.cpp
@@ -24,7 +24,8 @@ IfStmt::~IfStmt(void)
 	is->condition = Expression::create(mtd, stmt->thenPart->first->cond);
 
 	if ( !is->condition ) {
-		success = 0;
+		delete is;
+		return 0;
 	} else {
 	
 		if ( is->condition->expressionType()->isVoid() ) {
@@ -92,57 +93,64 @@ IfStmt::~IfStmt(void)
 
 	int if_start = 0, goto_start = 0;
 
-	ByteCode* code = new ByteCode();
+	ByteCode code;
 
-	code->log("Calculate condition");
-	this->condition->toByteCode(*code);
+	code.log("Calculate condition");
+	this->condition->toByteCode(code);
 
 	//если результат condition false переходим к (1)
 	//	(так как там BOOLEAN, можно ли просто сравнить результат с 0?)
-	code->ifeq(0x0666);	//пока тут мусор
-	if_start = code->currentOffset - 2; //заполмнили место откуда перезапишем (1)
+	if_start = code.currentOffset; //заполмнили место откуда перезапишем (1)
+	code.ifeq(0x0666);	//пока тут мусор
 
-	code->log("Write if-block to byte-code");
-	this->ifBody->toByteCode(*code);
+	code.log("Write if-block to byte-code");
+	this->ifBody->toByteCode(code);
 
 	//если это просто if
 	if ( !elseif && !elseBody ) {
 		
 		//вроде больше ничего не пишем
+		int length = +(code.currentOffset - if_start);
+		code.gotoPos(if_start+1)
+			.u2(length)
+			.gotoEnd();
 	}
 	else if ( elseif && elseBody) {
-		code->log("Very-very bad");		
+		code.log("Very-very bad");
+
+		//ошибка
+
 	} else {
 		
 		//пишем goto
-		code->goto_(0x0000); //пока мусор
-		goto_start = code->currentOffset - 2;
+		goto_start = code.currentOffset;		
+		code.goto_(0x0000); //пока мусор
 
 		//вписываем (1) в условие if
-		int lenght = +(code->currentOffset - if_start - 1);
-		code->gotoPos(if_start)
+		int lenght = +(code.currentOffset - if_start);
+		code.gotoPos(if_start+1)
 			.u2(lenght)
 			.gotoEnd();
 
 		if ( !elseif && elseBody ) {
-			code->log("Write else-block to byte-code");
-			this->elseBody->toByteCode(*code);
+			code.log("Write else-block to byte-code");
+			this->elseBody->toByteCode(code);
 		}
 
 		if ( elseif && !elseBody ) {
 		
-			code->log("Write else-block to byte-code");
-			this->elseif->toByteCode(*code);
+			code.log("Write else-block to byte-code");
+			this->elseif->toByteCode(code);
 		}
 
 		//присываем байты смещения в goto
-		lenght = +(code->currentOffset - goto_start - 1);
-		code->gotoPos(goto_start)
+		lenght = +(code.currentOffset - goto_start);
+		code.gotoPos(goto_start+1)
 			.u2(lenght)
 			.gotoEnd();	
 	}
 
-	return bc.append(*code);
+	return bc.append(code);
 }
 
 /*
