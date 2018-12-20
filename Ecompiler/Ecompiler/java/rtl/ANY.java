c87289139ee2c7651f0abd221c485c1638735cfc
Polikarpova <policarpova97@mail.ru>
2018-12-20 21:58:03 +0300

    обновлённый ANY

--- Ecompiler/Ecompiler/java/rtl/ANY.java
+++ Ecompiler/Ecompiler/java/rtl/ANY.java
@@ -5,12 +5,15 @@ package rtl;
  * @author Пользователь
  */
 public class ANY extends java.lang.Object {
+    
     public ANY()
 	{
 		io = new CONSOLEIO();
 	}
 	
     public CONSOLEIO io;
-	
-	
+    
+    public boolean _1_NotE(boolean left) {
+        return !left;
+    }
 }
